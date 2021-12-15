58d637815cab235c8fb0528dbc6055e9e825df79
Jerry Cheng <jerryycheng@gmail.com>
2021-12-15 16:23:15 -0500

    Change server endpoint

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://jerryc96-graphql-server.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
